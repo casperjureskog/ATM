f880b8de30da9ac3bcc7e73daab5b4394352a654
casperjureskog <lcasper@mac.com>
2017-02-06 20:44:42 +0100

    adds the good path to the atm dokuments

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,27 @@ class Atm
   def initialize
     @funds = 1000
   end
+
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account) then
+      return
+    else
+      preform_transaction(amount, account)
+    end
+  end
+
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def preform_transaction(amount, account)
+     @funds -= amount
+     account.balance = account.balance -amount
+     { status: true, message: 'success', date: Date.today, amount: amount}
+
+    end
+
 end

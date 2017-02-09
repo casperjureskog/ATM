5ef2d966992c936316cfc16fa488432057af6f89
casperjureskog <lcasper@mac.com>
2017-02-09 09:48:53 +0100

    solves the last to errors in Person.rb and person_rspec.rb

--- lib/person.rb
+++ lib/person.rb
@@ -8,57 +8,41 @@ class Person
   @account = nil
   end
 
-  def set_name(obj)
-     obj == nil ? missing_name : @name = 'casper'
-  end
-
-  def missing_name
-    raise "A name is required"
-  end
-
 
   def create_account
-    @account = Account.new(owner: self)
+   @account = Account.new(owner: self)
   end
-
-
 
   def deposit(amount)
     @account == nil ? missing_account : deposit_funds(amount)
   end
 
-  def withdraw(args = {})
-    @account == nil ? missing_account : withdraw_funds(args)
-  end
+
 
 
 
   private
 
-   def deposit_funds(amount)
+  def deposit_funds(amount)
      @cash -= amount
      @account.balance += amount
-   end
+  end
 
-   def increase_cash(response)
+  def increase_cash(response)
       @cash += response[:amount]
-    end
+  end
 
-#  def withdraw_funds(args)
-#   args[:atm] == nil ? missing_atm : atm = args[:atm]
-#   account = @account
-#   amount = args[:amount]
-#   pin = args[:pin]
-#   response = atm.withdraw(amount, pin, account)
-#   response[:status] == true ? increase_cash(response) : response
-# end
-
-
-
- def set_name(name)
+  def set_name(name)
    name == nil ? missing_name : name
- end
+  end
 
+  def missing_name
+    raise ArgumentError, 'A name is required'
+  end
+
+  def missing_account
+     raise RuntimeError, 'No account present'
+  end
 
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -33,19 +33,19 @@ describe Person do
      end
    end
 
-    describe 'can not manage funds if an account been created' do
-      let(:atm) { Atm.new }
-      before { subject.create_account }
-      it 'can deposit funds' do
-        expect(subject.deposit(100)).to be_thuthy
-      end
+  describe 'can manage funds if an account been created' do
+     let(:atm) { Atm.new }
+  before { subject.create_account }
+    it 'can deposit funds' do
+      expect(subject.deposit(100)).to be_truthy
     end
+  end
 
-    #describe 'can not manage funds if no account been created' do
-    #  it 'can\'t deposit funds' do
-    #    expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
-    #  end
-  #  end
+  describe 'can not manage funds if no account been created' do
+    it 'can\'t deposit funds' do
+      expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
+    end
+  end
 
 
   end
